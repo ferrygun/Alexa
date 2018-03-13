39ce500e0ddbecd2cee719cfed43d034136c32cb
Ferry Djaja <ferrygun@yahoo.com>
2018-03-13 11:48:19 +0800

    Update pptcontroller.js

--- pptcontroller.js
+++ pptcontroller.js
@@ -13,18 +13,30 @@ const port = 3000
 
 const requestHandler = (request, response) => {  
   console.log(request.url)
+  
   if(request.url.indexOf("next") !== -1){
     console.log("next slide");
 	slideshow.boot()
 	.then(function () { slideshow.start() })
     .then(function () { slideshow.next() })
   }
+  
   if(request.url.indexOf("previous") !== -1){
     console.log("previous slide");
 	slideshow.boot()
     .then(function () { slideshow.start() })
-   .then(function () { slideshow.prev() })
+    .then(function () { slideshow.prev() })
   }
+  
+  if(request.url.indexOf("slide") !== -1){
+	let slideno = request.url.split('=')[1];
+    console.log("go to slide: " + slideno);
+	slideshow.boot()
+    .then(function () { slideshow.start() })
+    .then(function () { slideshow.goto(slideno) })
+	
+  }
+
   response.end('Alexa PowerPoint Controller')
 }
 
